ce25e5a295580b72882a0a66bc696274c1986070
Sagar Patel <Sagar@AppLogics.uk>
2018-09-12 07:00:28 +0100

    Initial Commit

--- SwiftAsyncTaskWithoutCompletionHandler/ViewController.swift
+++ SwiftAsyncTaskWithoutCompletionHandler/ViewController.swift
@@ -9,17 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func startTouchUpInside(_ sender: Any) {
+        let url = URL(string: "https://jsonplaceholder.typicode.com/todos/1")!
+        let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+            guard let data = data else { return }
+            guard let jsonString = String(data: data, encoding: .utf8) else { return }
+            DispatchQueue.main.async {
+                self.setResults(text: jsonString);
+            }
+        }
+        task.resume()
+    }
+    
+    func setResults(text: String?){
+        textView.text = text;
+    }
+    
 }
-
